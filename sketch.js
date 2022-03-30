c839a5fdc31197eef250a6c182aeae595250c405
MariliaByjus <101297441+MariliaByjus@users.noreply.github.com>
2022-03-30 10:24:04 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var sea;
+sea=createSprite(200,200,20,20);
+function preload(){
+
+sea.addImage("sea.png");
+
+
+}
+
+function setup(){
+  createCanvas(400,400);
+  
+}
+
+function draw() {
+  background("blue");
+    drawSprites();
+
+ 
+}
